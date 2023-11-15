17d4570076b50160c4c2494ad994708d2f54d980
viduedo <37254701+viduedo@users.noreply.github.com>
2023-11-15 11:50:22 -0300

    Implantação de SEO

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,61 @@
 import "@/styles/globals.css"
 import type { AppProps } from "next/app"
 import { Analytics } from "@vercel/analytics/react"
+import { NextSeo } from "next-seo"
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
-      <Component {...pageProps} />
+      <NextSeo
+        title="Bemware: Soluções em TI"
+        description="Empresa especializada em tecnologia da informação"
+        canonical="https://www.bemware.com.br/"
+        openGraph={{
+          type: "website",
+          locale: "pt_BR",
+          url: "https://www.bemware.com.br/",
+          siteName: "Bemware: Soluções em TI",
+          images: [
+            {
+              url: "https://www.bemware.com.br/logo.png",
+              alt: "Logo",
+              type: "image/png",
+            },
+            {
+              url: "https://www.bemware.com.br/hero.png",
+              alt: "Hero",
+              type: "image/png",
+            },
+            {
+              url: "https://www.bemware.com.br/brands/aws.png",
+              alt: "Amazon AWS",
+              type: "image/png",
+            },
+            {
+              url: "https://www.bemware.com.br/brands/dell.png",
+              alt: "Dell",
+              type: "image/png",
+            },
+            {
+              url: "https://www.bemware.com.br/brands/microsoft.png",
+              alt: "Microsoft",
+              type: "image/png",
+            },
+            {
+              url: "https://www.bemware.com.br/brands/titan.png",
+              alt: "Titan",
+              type: "image/png",
+            },
+            {
+              url: "https://www.bemware.com.br/aboutus.jpg",
+              alt: "Quem Somos",
+              type: "image/jpeg",
+            },
+          ],
+        }}
+      />
       <Analytics />
+      <Component {...pageProps} />
     </>
   )
 }
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -2,8 +2,11 @@ import { Html, Head, Main, NextScript } from "next/document"
 
 export default function Document() {
   return (
-    <Html lang="pt-br">
-      <Head />
+    <Html lang="pt-BR">
+      <Head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width" />
+      </Head>
       <body>
         <Main />
         <NextScript />
